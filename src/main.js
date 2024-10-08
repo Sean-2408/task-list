838dd1de2e2582fa2476a3967112548b974dc0f6
Sean Ballantyne <ballantyne.sean@gmail.com>
2024-10-08 15:39:31 +0200

    Added flex functionality. Added delete buttons to content entries.

--- src/main.js
+++ src/main.js
@@ -1,45 +1,55 @@
-const data = [todo];
+const completedContainer = document.getElementById('todo');
+const data = [];
 data.forEach(item => {
     createListItem(item);
 });
 
 // This is the div element that has the id=completed
-//const completedContainer = document.getElementById('todo');
 
-function createListItem(todo){
+function createListItem(todo) {
     const div = document.createElement('div');
+    const checkboxDiv = document.createElement('div');
     // Here we construct an input element eg: <label></label>
+    div.className = 'flex-container'
     const label = document.createElement('label');
     // Here we construct an input element eg: <input>
     const input = document.createElement('input');
     // Here we specify the type of input eg: <input type="checkbox">
     input.type = 'checkbox';
-    input.id = title;
-    label.innerText = task;
-    input.name = task;
-    label.htmlFor =task;
+    input.id = todo;
+    label.innerText = todo;
+    input.name = todo;
+    label.htmlFor = todo;
+    const button = document.createElement('button');
+    // Here we specify the type of input eg: <input type="checkbox">
+    button.id = `${todo}_btn_delete`;
+    button.innerText = 'Del';
+    button.className = 'btn_delete'
     /* Here we add that checkbox to the div, eg: 
     <div> <-- This is the completedContainer
         <label>{{title}}</label>
         <input type="checkbox">
     </div>
     */
-    div.append(label, input);
+    checkboxDiv.append(input, label);
+    div.append(checkboxDiv, button);
     completedContainer.append(div);
 }
 
 
-const submit = favDialog.querySelector("#submit")
-submit.addEventlistener
-function addItemToDo(itemName){
+// const submit = favDialog.querySelector("#submit")
+// submit.addEventlistener
+function addItemToDo(itemName) {
     data.push(itemName);
     createListItem(itemName);
 }
 
-    function textPrompt() {
-        let task = prompt("Task Name:", "");
-        if (task != null) {
-          document.getElementById("todo").innerHTML =task;
+function textPrompt() {
+    let task = prompt("Task Name:", "");
+    if (task != null) {
+        // document.getElementById("todo").innerHTML = task;
+        createListItem(task)
 
-        }
-      }
+    }
+}
+
